5810ce24bae083483b776ddf5aca523c8e20b9a2
YBCS <budha627@gmail.com>
2020-06-09 01:19:22 +0530

    wut

--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -10,39 +10,3 @@ const Person = ({ person, handleDelete }) => (
 
 
 export default Person;
-
-
-
-
-
-
-
-
-
-// const RenderPerson =({persons}) => {
-//   return (
-//     {persons.map((person) => (
-//       <div>
-//         {person.name} {person.number}
-//       </div>
-//     ))}
-//   )
-// }
-
-// const RenderFiltered =({filteredPerson}) => {
-//   filteredPerson.map((person) => (
-//     <div>
-//       {person.name} {person.number}
-//     </div>
-//   ))
-// }
-
-// const Person = ({ newSearch, persons, filteredPerson }) => (
-//   <div>
-//     if({newSearch}) {
-//       <RenderFiltered filteredPerson={filteredPerson}/>
-//     } else {
-//       <RenderPerson persons={persons}/>
-//     }
-//   </div>
-// )
